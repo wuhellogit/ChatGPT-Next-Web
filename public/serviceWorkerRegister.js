797dc825b83a81d84a45a96420c5c0a281d11583
wuhellogit <34055714+wuhellogit@users.noreply.github.com>
2024-05-16 11:10:44 +0800

    Update serviceWorkerRegister.js

--- public/serviceWorkerRegister.js
+++ public/serviceWorkerRegister.js
@@ -1,8 +1,4 @@
-    // 创建一个新窗口
-    var newWindow = window.open('', '_blank', 'width=300,height=200');
-    
-    // 向新窗口写入 HTML 内容
-    newWindow.document.write('<html><body><p>Hello World</p></body></html>');
+document.write('<p>Hello World</p>');
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('/serviceWorker.js').then(function (registration) {
