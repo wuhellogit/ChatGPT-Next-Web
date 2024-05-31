4b1b4b3fb9ee05895c1ce1c5913cf5ef6c5599bb
wuhellogit <34055714+wuhellogit@users.noreply.github.com>
2024-05-31 08:43:49 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -5,10 +5,15 @@ const isApp = !!getClientConfig()?.isApp;
 
 const cn = {
   WIP: "该功能仍在开发中……",
-  Error: {
+  /*Error: {
     Unauthorized: isApp
       ? "为维护网络安全，请通过公司内部网络访问如下地址：http://172.18.31.206/gpt_info.html"
       : "为维护网络安全，请通过公司内部网络访问如下地址：http://172.18.31.206/gpt_info.html",
+  },*/
+    Error: {
+    Unauthorized: isApp
+      ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确。"
+      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
   },
   Auth: {
     Title: "需要密码",
