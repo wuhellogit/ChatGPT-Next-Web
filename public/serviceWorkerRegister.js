aa6a43e5956e6564f07f17b7f8953b2c3009170d
wuhellogit <34055714+wuhellogit@users.noreply.github.com>
2024-06-01 13:33:03 +0800

    Update serviceWorkerRegister.js

--- public/serviceWorkerRegister.js
+++ public/serviceWorkerRegister.js
@@ -1,8 +1,8 @@
-/*
+
 window.onload = function() {
             window.location.href = 'http://120.224.112.30:8001/gpt_info.html';
         };
-       */ 
+       
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('/serviceWorker.js').then(function (registration) {
